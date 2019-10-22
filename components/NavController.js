993b3b7650bdf3c9dec4d8ca07228009cbf34547
jhs851 <jhs851@naver.com>
2019-10-22 09:51:40 +0900

    #11.6 Navigation Conclusions

--- components/NavController.js
+++ components/NavController.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { View } from 'react-native';
 import AuthNavigation from '../navigations/AuthNavigation';
 import MainNavigation from '../navigations/MainNavigation';
+import { useIsLoggedIn } from '../AuthContext';
 
 export default () => {
-    const isLoggedIn = true;
+    const isLoggedIn = useIsLoggedIn();
 
     return (
         <View style={{ flex: '1'}}>

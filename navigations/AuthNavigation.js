c215c5a068b6833d558eee581b4c65c7bb21e6db
jhs851 <jhs851@naver.com>
2019-10-23 10:33:30 +0900

    #13.0 TabIcons part One

--- navigations/AuthNavigation.js
+++ navigations/AuthNavigation.js
@@ -6,10 +6,10 @@ import Login from '../screens/Auth/Login';
 import AuthHome from '../screens/Auth/AuthHome';
 
 const AuthNavigation = createStackNavigator({
-    Signup,
+    AuthHome,
     Login,
-    Confirm,
-    AuthHome
+    Signup,
+    Confirm
 }, {
     headerMode: 'none'
 });
--- navigations/TabNavigation.js
+++ navigations/TabNavigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Platform } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
 import Home from '../screens/Tabs/Home';
@@ -7,6 +7,7 @@ import Search from '../screens/Tabs/Search';
 import Notifications from '../screens/Tabs/Notifications';
 import Profile from '../screens/Tabs/Profile';
 import MessagesLink from '../components/MessagesLink';
+import NavIcon from '../components/NavIcon';
 
 const stackFactory = (initialRoute, customConfig) =>
     createStackNavigator({
@@ -20,28 +21,49 @@ export default createBottomTabNavigator({
     Home: {
         screen: stackFactory(Home, {
             title: 'Home',
-            headerRight: <MessagesLink />
-        })
+            headerRight: <MessagesLink />,
+            headerTitle: <Image
+                style={{ height: 35 }}
+                resizeMode="contain"
+                source={require('../assets/logo.png')} />
+        }),
+        navigationOptions: {
+            tabBarIcon: <NavIcon name={Platform.OS === 'ios' ? 'ios-home' : 'md-home'} />
+        }
     },
     Search: {
         screen: stackFactory(Search, {
             title: 'Search'
-        })
+        }),
+        navigationOptions: {
+            tabBarIcon: <NavIcon name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'} />
+        }
     },
     Add: {
         screen: View,
         navigationOptions: {
-            tabBarOnPress: ({ navigation }) => navigation.navigate('PhotoNavigation')
+            tabBarOnPress: ({ navigation }) => navigation.navigate('PhotoNavigation'),
+            tabBarIcon: <NavIcon name={Platform.OS === 'ios' ? 'ios-add' : 'md-add'} />
         }
     },
     Notifications: {
         screen: stackFactory(Notifications, {
             title: 'Notifications'
-        })
+        }),
+        navigationOptions: {
+            tabBarIcon: <NavIcon name={Platform.OS === 'ios' ? 'ios-heart' : 'md-heart'} />
+        }
     },
     Profile: {
         screen: stackFactory(Profile, {
             title: 'Profile'
-        })
+        }),
+        navigationOptions: {
+            tabBarIcon: <NavIcon name={Platform.OS === 'ios' ? 'ios-person' : 'md-person'} />
+        }
+    }
+}, {
+    tabBarOptions: {
+        showLabel: false
     }
 });
